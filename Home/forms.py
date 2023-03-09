fafd9a6115f7e6e406e77edecc8e6c55bea70b4d
Juan <jlizarraga@unomaha.edu>
2023-03-09 14:02:00 -0600

    commit

--- Home/forms.py
+++ Home/forms.py
@@ -2,7 +2,10 @@ from django import forms
 
 
 class ContactMe(forms.Form):
-
+    email = forms.EmailField(
+        label=("Email"),
+        widget=forms.EmailInput
+    )
     subject = forms.CharField(max_length=50)
     message = forms.CharField(widget=forms.Textarea())
 
